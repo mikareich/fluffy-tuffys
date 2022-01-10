c78eed06401368936d0b51592aa0027c10d44f84
Mika Reich <mikareich0@gmail.com>
2022-01-10 21:44:19 +0100

    update tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,16 @@
+import '../styles/index.css'
+
 import React from 'react'
 import type { AppProps } from 'next/app'
 import NavBar from '../components/NavBar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className="w-screen h-screen">
       <NavBar />
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <Component {...pageProps} />
-    </>
+    </div>
   )
 }
 

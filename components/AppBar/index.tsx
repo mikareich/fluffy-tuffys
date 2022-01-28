34a248f5cc894e4772d72544e6e4ffd7d006ae1d
Mika Reich <mikareich0@gmail.com>
2022-01-28 21:23:20 +0100

    reset all

--- components/AppBar/index.tsx
+++
@@ -1,5 +0,0 @@
-import Container from './Container'
-import Logo from './Logo'
-import MenuItem from './MenuItem'
-
-export default { Container, MenuItem, Logo }
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable global-require */
-/* eslint-disable import/no-extraneous-dependencies */
-module.exports = {
-  content: ['./pages/**/*.tsx', './components/**/*.tsx'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/typography')],
-}
